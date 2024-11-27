b4e13b9e20912b1311766ad4f2d18c0a680400ff
Drowning Cat <shamal.ma.personal@gmail.com>
2024-11-27 14:26:41 +0300

    feat: update keymaps for nvim-cmp
    <C-d>     - scroll down
    <C-u>     - scroll up
    <C-j>     - select_next_item
    <C-k>     - select_prev_item
    <C-c>     - abort
    <C-Space> - complete_or_confirm
    
    -- <Enter>   - complete
    -- <Tab>     - select_next_item
    -- <S+Tab>   - select_prev_item

--- init.lua
+++ init.lua
@@ -760,6 +760,14 @@ require('lazy').setup({
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
 
+      local complete_or_confirm = function()
+        if not cmp.visible() then
+          cmp.complete { select = true }
+        else
+          cmp.confirm {}
+        end
+      end
+
       cmp.setup {
         snippet = {
           expand = function(args)
@@ -775,12 +783,14 @@ require('lazy').setup({
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
           ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
@@ -789,14 +799,17 @@ require('lazy').setup({
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          -- ['<CR>'] = cmp.mapping.confirm { select = true },
+          -- ['<Tab>'] = cmp.mapping.select_next_item(),
+          -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+          -- Abort current completion
+          ['<C-c>'] = cmp.mapping.abort(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-Space>'] = complete_or_confirm,
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:

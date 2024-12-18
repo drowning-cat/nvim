9a8a0d1a2f2f4f95c68f0088043c70908cebb534
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-18 18:43:44 +0300

    REFACTOR!: split init.lua into individual files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,5 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
+-- Neovim plugin to browse the file system
+--
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',

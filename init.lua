3084f1986a35f703bba72c3cfcb1df5a8f02fb8d
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-18 20:54:51 +0300

    feat: disable icon mappings for which-key

--- init.lua
+++ init.lua
@@ -218,7 +218,7 @@ require('lazy').setup({
     opts = {
       icons = {
         -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
+        mappings = false, -- vim.g.have_nerd_font,
         -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
         -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {

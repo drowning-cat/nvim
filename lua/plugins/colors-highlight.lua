be91e5c3c333d87308cd6f412913c60793e84137
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-19 22:56:57 +0300

    feat: display colors inline using nvim-highlight-colors

---
+++ lua/plugins/colors-highlight.lua
@@ -0,0 +1,17 @@
+return {
+  'brenoprata10/nvim-highlight-colors',
+  event = 'VeryLazy',
+  opts = {
+    ---@usage 'background'|'foreground'|'virtual'
+    render = 'virtual',
+    ---@usage 'inline'|'eol'|'eow'
+    virtual_symbol_position = 'eow',
+    virtual_symbol = '■',
+    virtual_symbol_prefix = '',
+    virtual_symbol_suffix = ' ',
+  },
+  -- stylua: ignore
+  keys = {
+    { '<leader>tc', function() require('nvim-highlight-colors').toggle() end, desc = '[T]oggle [C]colors' },
+  },
+}

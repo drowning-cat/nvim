0dfe00750eae18eee22d771105c0ae4ee92b05a7
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-19 17:58:43 +0300

    refactor: enable huez.nvim for managing themes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+-- To see already installed colorschemes use `:Telescope colorscheme`
+
+return {
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   lazy = true,
+  --   opts = {
+  --     commentStyle = { italic = false },
+  --     keywordStyle = { italic = false },
+  --   },
+  -- },
+
+  { -- Theme manager
+    'vague2k/huez.nvim',
+    import = 'huez-manager.import',
+    event = 'UIEnter',
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { '<leader>co', function() require('huez.pickers').themes() end, desc = '[Co]lorscheme' },
+      { '<leader>col', function() require('huez.pickers').live() end, desc = '[Co]lorscheme [L]ive' },
+      { '<leader>coe', function() require('huez.pickers').ensured() end, desc = '[Co]lorscheme [E]nsured' },
+      { '<leader>cof', function() require('huez.pickers').favorites() end, desc = '[Co]lorscheme [F]avorites' },
+    },
+  },
+}

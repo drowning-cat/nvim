4928cff0f2e54372aeb2e9bdf119d0ffd3b668ce
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-20 19:52:48 +0300

    feat: enable multicursor support using multicursor.nvim
    * <S-Up>        n, v - Add a new cursor above the main one
    * <S-Down>      n, v - Add a new cursor below the main one
    * <S-Right>     n, v - Jump to the next cursor
    * <S-Left>      n, v - Jump to the previous cursor
    * <leader>n     n, v - Add a new cursor below by matching word/selection
    * <leader>N     n, v - Add a new cursor above by matching word/selection
    * <leader>s     n, v - Skip (remove) the cursor below by matching word/selection
    * <leader>S     n, v - Skip (remove) the cursor above by matching word/selection
    * <leader>x     n, v - Remove the main cursor
    * <C-leftmouse> n    - Add/remove the cursor with mouse click
    * <C-Space>     n, v - Add/remove the cursor using the main one
    * <leader>gv    n    - Bring back cursors if you accidentally clear them
    * M             v    - Match new cursors within visual selections by regex
    * <leader>t     v    - Transpose the lines in the visual selection contents
    * <leader>T     v    - Transpose the lines in the visual selection backward
    
    * <Esc> - Enable or clear cursors
    * <C-i> - Jumplist support
    * <C-o> - Jumplist support

---
+++ lua/plugins/multicursor.lua
@@ -0,0 +1,76 @@
+--- @param dir -1|1 Direction
+local lineAddCursor = function(dir)
+  local mc = require 'multicursor-nvim'
+  local is_first, is_last
+  mc.action(function(ctx)
+    local active = { enabledCursors = true }
+    is_first = ctx:mainCursor() == ctx:firstCursor(active)
+    is_last = ctx:mainCursor() == ctx:lastCursor(active)
+  end)
+  if is_first ~= is_last and ((is_first and dir == 1) or (is_last and dir == -1)) then
+    mc.deleteCursor()
+  else
+    mc.lineAddCursor(dir)
+  end
+end
+
+return {
+  {
+    'jake-stewart/multicursor.nvim',
+    config = function()
+      local mc = require 'multicursor-nvim'
+
+      mc.setup()
+
+      vim.api.nvim_set_hl(0, 'MultiCursorCursor', { link = 'Cursor' })
+      vim.api.nvim_set_hl(0, 'MultiCursorVisual', { link = 'Visual' })
+      vim.api.nvim_set_hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+      vim.api.nvim_set_hl(0, 'MultiCursorDisabledCursor', { link = 'Visual' })
+      vim.api.nvim_set_hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
+      vim.api.nvim_set_hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+
+      --- @param mode string|string[]
+      --- @param lhs string
+      --- @param fn function
+      --- @param opts? vim.keymap.set.Opts
+      local map = function(mode, lhs, fn, opts)
+        local blocked_filetypes = { ['neo-tree'] = true }
+        vim.keymap.set(mode, lhs, function()
+          if not blocked_filetypes[vim.bo.filetype] then
+            fn()
+          end
+        end, opts)
+      end
+
+      map({ 'n' }, '<Esc>', function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        elseif mc.hasCursors() then
+          mc.clearCursors()
+        else
+          vim.cmd [[ nohlsearch ]]
+        end
+      end, { desc = 'Enable or clear cursors' })
+
+      -- stylua: ignore start
+      map({'n','v'}, '<S-Up>', function() lineAddCursor(-1) end, { desc = 'Add a new cursor above' })
+      map({'n','v'}, '<S-Down>', function() lineAddCursor(1) end, { desc = 'Add a new cursor below' })
+      map({'n','v'}, '<S-Right>', function() mc.nextCursor() end, { desc = 'Jump to the next cursor' })
+      map({'n','v'}, '<S-Left>', function() mc.prevCursor() end, { desc = 'Jump to the previous cursor' })
+      map({'n','v'}, '<leader>n', function() mc.matchAddCursor(1) end, { desc = 'Add a [n]ew cursor below by matching' })
+      map({'n','v'}, '<leader>N', function() mc.matchAddCursor(-1) end, { desc = 'Add a [N]ew cursor above by matching' })
+      map({'n','v'}, '<leader>s', function() mc.matchSkipCursor(1) end, { desc = '[s]kip the cursor below by matching' })
+      map({'n','v'}, '<leader>S', function() mc.matchSkipCursor(-1) end, { desc = '[S]kip the cursor above by matching' })
+      map({'n','v'}, '<leader>x', function() mc.deleteCursor() end, { desc = 'Remove the main cursor' })
+      map({'n','v'}, '<C-Space>', function() mc.toggleCursor() end, { desc = 'Add or remove the cursor' })
+      map({'n'}, '<C-leftmouse>', function() mc.handleMouse() end, { desc = 'Add or remove the cursor' })
+      map({'n'}, '<leader>gv', function() mc.restoreCursors() end, { desc = 'Bring back cursors if you accidentally clear them' })
+      map({'v'}, 'M', function() mc.matchCursors() end, { desc = '[M]atch new cursors within visual selections by regex' })
+      map({'v'}, '<leader>t', function() mc.transposeCursors(1) end, { desc = '[t]ranspose lines in the visual selection' })
+      map({'v'}, '<leader>T', function() mc.transposeCursors(-1) end, { desc = '[T]ranspose lines in the visual selection backward' })
+      map({'n','v'}, '<C-i>', function() mc.jumpForward() end)
+      map({'n','v'}, '<C-o>', function() mc.jumpBackward() end)
+      -- stylua: ignore end
+    end,
+  },
+}

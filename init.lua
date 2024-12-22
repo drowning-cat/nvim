dd42941c9fb9e1e744c002f807e12b9072d369ef
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-22 18:33:29 +0300

    refactor: remove kickstart from the project

--- init.lua
+++ init.lua
@@ -104,7 +104,7 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- Highlight when copying text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,

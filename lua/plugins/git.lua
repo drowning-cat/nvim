b7a638585013f98cc8712f9b6c2565e91c7c2e7d
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-28 23:57:21 +0300

    feat: update gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,23 +4,16 @@ return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
       current_line_blame_opts = {
         delay = 0,
       },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
-        local function map(mode, l, r, opts)
+        local function map(mode, lhs, rhs, opts)
           opts = opts or {}
           opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
+          vim.keymap.set(mode, lhs, rhs, opts)
         end
 
         -- Navigation: previous hunk
@@ -41,23 +34,24 @@ return {
           end
         end, { desc = 'Goto prev [h]unk *change' })
 
+        map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[G]it [s]tage hunk' })
+        map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[G]it [r]eset hunk' })
         -- stylua: ignore start
-        map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'Git [h]unk [s]tage' })
-        map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'Git [h]unk [r]eset' })
+        map('v', '<leader>gs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[G]it [s]tage hunk' })
+        map('v', '<leader>gr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[G]it [r]eset hunk' })
         -- stylua: ignore end
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Git [s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Git [r]eset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Git [S]tage buffer' })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Git [u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Git [R]eset buffer' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Git [p]review hunk' })
-        map('n', '<leader>hP', gitsigns.preview_hunk_inline, { desc = 'Git [P]review hunk inline' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'Git [b]lame line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Git [d]iff against index' })
+        map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = '[G]it [u]ndo stage hunk' })
+        map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[G]it [S]tage buffer' })
+        map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[G]it [R]eset buffer' })
+        map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[G]it [p]review hunk' })
+        map('n', '<leader>gP', gitsigns.preview_hunk_inline, { desc = '[G]it [P]review hunk inline' })
+        map('n', '<leader>gb', gitsigns.blame_line, { desc = '[G]it [b]lame line' })
+        map('n', '<leader>gd', gitsigns.diffthis, { desc = '[G]it [d]iff against index' })
         -- stylua: ignore
-        map('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'Git [D]iff against last commit' })
+        map('n', '<leader>gD', function() gitsigns.diffthis '@' end, { desc = '[G]it [D]iff against last commit' })
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git [b]lame line' })
         map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git [D]eleted' })
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '[G]it select hunk' })
       end,
     },
   },

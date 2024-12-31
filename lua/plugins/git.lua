038983388cad9d0b4af0b2046ae36c21a166075a
Drowning Cat <shamal.ma.personal@gmail.com>
2024-12-31 19:25:36 +0300

    feat: enable lazygit.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,24 @@
 return {
   'tpope/vim-fugitive', -- Vim integration with git, see `:help fugitive`
 
+  { -- Open lazygit - a lightweight, terminal user interface for git
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {

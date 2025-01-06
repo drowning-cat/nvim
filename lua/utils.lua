3a60a79598bde5ad406f7a41f51ee55a9e1971d1
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-07 02:26:43 +0300

    feat: extend utils with notify()

--- lua/utils.lua
+++ lua/utils.lua
@@ -29,6 +29,34 @@ function M.list_remove_dups_mut(list)
   end
 end
 
+--- @class NotifyOpts
+--- @field duration? integer: Time in milliseconds to show the notification.
+--- @field force? boolean: Whether clear after duration over active notification.
+
+--- @param message string: The notification message to display.
+--- @param opts? NotifyOpts: Settings for the notification.
+--- @description A wrapper around `vim.notify` that adds support for delayed notifications.
+function M.notify(message, opts)
+  opts = vim.tbl_extend('keep', opts or {}, {
+    duration = nil,
+    force = true,
+  })
+
+  local get_last_message = function()
+    return vim.fn.execute '1messages'
+  end
+
+  vim.notify(message)
+
+  if opts.duration then
+    vim.defer_fn(function()
+      if opts.force or get_last_message() == message then
+        vim.notify ''
+      end
+    end, opts.duration)
+  end
+end
+
 --- @description Assigns utility functions to `vim.u`, `vim.util` namespaces
 function M.setup()
   vim.u = M

d0de1c1d7e10778e92d1e2458f32c512a162ab58
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-07 04:21:05 +0300

    feat: extend telescope keymaps with lsp-specific pickers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -80,13 +80,19 @@ return {
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sl', builtin.live_grep, { desc = '[S]earch [L]ive grep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s/', builtin.live_grep, { desc = '[S]earch Live Grep' })
+      vim.keymap.set('n', '<leader>s;', builtin.live_grep, { desc = '[S]earch Live Grep' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+      vim.keymap.set('n', '<leader>sp', builtin.spell_suggest, { desc = '[S]earch S[p]elling' })
+      vim.keymap.set('n', '<leader>slr', builtin.lsp_references, { desc = '[S]earch [L]sp [R]eferences' })
+      vim.keymap.set('n', '<leader>sls', builtin.lsp_document_symbols, { desc = '[S]earch [L]sp [S]ymbols' })
+      vim.keymap.set('n', '<leader>slws', builtin.lsp_workspace_symbols, { desc = '[S]earch [L]sp [W]orkspace [S]ymbols' })
+      vim.keymap.set('n', '<leader>slt', builtin.lsp_type_definitions, { desc = '[S]earch [L]sp [T]ype definitions' })
       vim.keymap.set('n', '<leader>sgb', builtin.git_branches, { desc = '[S]earch [G]it [B]ranches' })
       vim.keymap.set('n', '<leader>sgc', builtin.git_commits, { desc = '[S]earch [G]it [C]ommits' })
       vim.keymap.set('n', '<leader>sgf', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
@@ -99,15 +105,6 @@ return {
           previewer = false,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()

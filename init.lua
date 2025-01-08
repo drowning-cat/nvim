6ed0f4c5367e759170bc22c34c092d01eafba95d
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-08 17:58:22 +0300

    fix: paste linewise in normal mode

--- init.lua
+++ init.lua
@@ -29,6 +29,40 @@ vim.keymap.set({ 'n', 'v' }, '<leader>D', '"+D')
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p')
 vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P')
 
+---@param lines string[]
+local mut_trim_newline = function(lines)
+  if lines[#lines] == '' then
+    table.remove(lines, #lines)
+  end
+end
+
+vim.paste = (function(overridden)
+  return function(lines, phase)
+    local mode = vim.fn.mode()
+    mut_trim_newline(lines)
+    if mode == 'n' then
+      vim.api.nvim_put(lines, 'l', true, false) -- p
+    elseif mode == 'V' then
+      vim.fn.execute [[ exe "silent normal! \<Del>" ]]
+      vim.api.nvim_put(lines, 'l', false, false) -- P
+      local select_end = vim.fn.getpos "']"
+      select_end[3] = 0 -- col = 0
+      vim.fn.setpos('.', select_end)
+    else
+      overridden(lines, phase)
+    end
+  end
+end)(vim.paste)
+
+-- Highlight when copying text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Enable break indent
 vim.opt.breakindent = true
 
@@ -162,15 +196,6 @@ vim.keymap.set('n', '<leader>tp', listchar_toggle_eol, { desc = '[T]oggle [P]ara
 vim.keymap.set('n', '<leader>tl', list_toggle, { desc = '[T]oggle [L]istchars' })
 vim.keymap.set('n', '<leader>tle', listchar_toggle_eol, { desc = '[T]oggle [L]istchar [E]ol' })
 vim.keymap.set('n', '<leader>tls', listchar_toggle_space, { desc = '[T]oggle [L]istchar [S]space' })
-
--- Highlight when copying text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- Install `lazy.nvim` plugin manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'

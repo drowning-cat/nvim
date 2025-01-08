75cf23cba7eb86efc0dfa88470e9297720927015
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-08 12:26:11 +0300

    feat: enhance markdown with peek.nvim, render-markdown.nvim

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    'toppair/peek.nvim',
+    cmd = { 'Mopen', 'Mclose' },
+    cond = function()
+      return vim.fn.executable 'deno' == 1
+    end,
+    build = 'deno task --quiet build:fast',
+    config = function()
+      local peek = require 'peek'
+      peek.setup()
+      vim.api.nvim_create_user_command('Mopen', peek.open, {})
+      vim.api.nvim_create_user_command('Mclose', peek.close, {})
+    end,
+  },
+
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = 'markdown',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.nvim',
+    },
+    opts = {
+      enabled = false,
+      code = {
+        sign = false,
+        width = 'block',
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+    config = function(_, opts)
+      require('render-markdown').setup(opts)
+      if Snacks then ---@module 'snacks'
+        Snacks.toggle({
+          name = '[m]arkdown',
+          get = function()
+            return require('render-markdown.state').enabled
+          end,
+          set = function(enabled)
+            require('render-markdown')[enabled and 'enable' or 'disable']()
+          end,
+        }):map '<leader>tm'
+      end
+    end,
+  },
+}

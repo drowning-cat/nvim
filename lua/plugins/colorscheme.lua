5b75052566f7d7bc49411e25a9a70a20fca793df
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-08 16:27:58 +0300

    refactor: merge some plugin files

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,7 +16,6 @@ return {
   --     keywordStyle = { italic = false },
   --   },
   -- },
-
   { -- Theme manager
     'vague2k/huez.nvim',
     import = 'huez-manager.import',
@@ -29,6 +28,29 @@ return {
       { '<leader>Hl', function() require('huez.pickers').live() end, desc = '[H]uez [L]ive' },
       { '<leader>He', function() require('huez.pickers').ensured() end, desc = '[H]uez [E]nsured' },
       { '<leader>Hf', function() require('huez.pickers').favorites() end, desc = '[H]uez [F]avorites' },
+    },
+  },
+  { -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  { -- Highlight colors in code
+    'brenoprata10/nvim-highlight-colors',
+    event = 'VeryLazy',
+    opts = {
+      ---@usage 'background'|'foreground'|'virtual'
+      render = 'virtual',
+      ---@usage 'inline'|'eol'|'eow'
+      virtual_symbol_position = 'eow',
+      virtual_symbol = '⚈',
+      virtual_symbol_prefix = ' ',
+      virtual_symbol_suffix = '',
+    },
+    -- stylua: ignore
+    keys = {
+      { '<leader>tc', function() require('nvim-highlight-colors').toggle() end, desc = '[T]oggle [C]colors' },
     },
   },
 }

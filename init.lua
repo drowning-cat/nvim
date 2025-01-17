01be5ce564abeba4a474ee88632f9486473de470
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-17 12:38:14 +0300

    feat: adopt LazyVim general settings

--- init.lua
+++ init.lua
@@ -21,58 +21,6 @@ vim.opt.showmode = false
 vim.opt.backupdir = vim.fn.stdpath 'state' .. '/backup'
 vim.opt.backup = true
 
--- Add timestamp as extension for backup files
-vim.api.nvim_create_autocmd('BufWritePre', {
-  group = vim.api.nvim_create_augroup('timestamp_backupext', { clear = true }),
-  desc = 'Add timestamp to backup extension',
-  pattern = '*',
-  callback = function()
-    vim.opt.backupext = '-' .. vim.fn.strftime '%Y%m%d%H%M'
-  end,
-})
-
--- Sync clipboard between OS and Neovim
--- vim.schedule(function()
---   vim.opt.clipboard = 'unnamedplus'
--- end)
-vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
-vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y')
-vim.keymap.set({ 'n', 'v' }, '<leader>c', '"+c')
-vim.keymap.set({ 'n', 'v' }, '<leader>C', '"+C')
-vim.keymap.set({ 'n', 'v' }, '<leader>d', '"+d')
-vim.keymap.set({ 'n', 'v' }, '<leader>D', '"+D')
-vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p')
-vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P')
-
-vim.paste = (function(overridden)
-  return function(lines, phase)
-    local mode = vim.fn.mode()
-    if lines[#lines] == '' then
-      table.remove(lines, #lines)
-    end
-    if mode == 'n' then
-      vim.api.nvim_put(lines, 'l', true, false) -- paste
-    elseif mode == 'V' then
-      vim.fn.execute [[ exe "silent normal! \<Del>" ]] -- Delete selection
-      vim.api.nvim_put(lines, 'l', false, false) -- Paste
-      local select_end = vim.fn.getpos "']" -- Get selection end-position
-      select_end[3] = 0 -- Move cursor to start of line
-      vim.fn.setpos('.', select_end)
-    else
-      overridden(lines, phase)
-    end
-  end
-end)(vim.paste)
-
--- Highlight when copying text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 -- Enable break indent
 vim.opt.breakindent = true
 
@@ -121,50 +69,80 @@ vim.opt.scrolloff = 10
 vim.opt.spelllang = 'en_us,ru'
 vim.opt.spell = true
 
--- Prevent certain keymaps from being printed:
--- stylua: ignore
-for _, lhs in ipairs {
-  'C-q', 'C-e', 'C-o', 'C-a', 'C-h', 'C-j', 'C-k', 'C-l', 'C-z', 'C-v', 'C-b', 'C-_', 'C-/', 'C-\\', 'S-Del',
-  'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
-  'C-F1', 'C-F2', 'C-F3', 'C-F4', 'C-F5', 'C-F6', 'C-F7', 'C-F8', 'C-F9', 'C-F10', 'C-F11', 'C-F12',
-} do
-  vim.keymap.set({ 'i', 'c' }, '<' .. lhs .. '>', '<nop>')
+-- Prevent ^F from being printed
+local indentexpr = vim.bo.indentexpr
+if indentexpr == '' then
+  vim.bo.indentexpr = 'v:lua.__indentexpr_unknown()'
 end
 
--- Add empty lines in normal mode
-vim.keymap.set('n', '<leader>O', "<cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>", { desc = 'Add blank line above' })
-vim.keymap.set('n', '<leader>o', "<cmd>call append(line('.'),     repeat([''], v:count1))<CR>", { desc = 'Add blank line below' })
+-- Prevent certain keymaps from being printed:
+local unmap_insert = function(lhs)
+  vim.keymap.set({ 'i', 'c' }, lhs, '<nop>')
+end
+for i = 1, 12 do
+  unmap_insert(string.format('<F%i>', i))
+  unmap_insert(string.format('<C-F%i>', i))
+end
+for _, key in ipairs { 'C-z', 'C-b', 'C-_', 'C-\\', 'S-Del' } do
+  unmap_insert(string.format('<%s>', key))
+end
 
--- Move selection up or down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
+-- Move virtual lines instead of physical
+vim.keymap.set({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+
+-- Move selected line below or above
+vim.keymap.set('v', 'J', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move line below' })
+vim.keymap.set('v', 'K', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move line above' })
 
 -- Move selection left or right
-vim.keymap.set('v', '<', '<gv', { noremap = true })
-vim.keymap.set('v', '>', '>gv', { noremap = true })
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
--- Additional insert motions using
+-- Add undo breakpoint
+vim.keymap.set('i', ',', ',<C-g>u')
+vim.keymap.set('i', '.', '.<C-g>u')
+vim.keymap.set('i', ';', ';<C-g>u')
+
+-- Add empty lines in normal mode
+vim.keymap.set({ 'n', 'v' }, '<leader>O', "<cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>", { desc = 'Add blank line above' })
+vim.keymap.set({ 'n', 'v' }, '<leader>o', "<cmd>call append(line('.'),     repeat([''], v:count1))<CR>", { desc = 'Add blank line below' })
+
+-- Mode independent search navigation
+vim.keymap.set('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next search result' })
+vim.keymap.set('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
+vim.keymap.set('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
+vim.keymap.set('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev search result' })
+vim.keymap.set('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
+vim.keymap.set('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
+
+-- Additional insert mode motions
 vim.keymap.set('i', '<C-S-h>', '<Left>', { noremap = true })
 vim.keymap.set('i', '<C-S-j>', '<Down>', { noremap = true })
 vim.keymap.set('i', '<C-S-k>', '<Up>', { noremap = true })
 vim.keymap.set('i', '<C-S-l>', '<Right>', { noremap = true })
---
 vim.keymap.set('i', '<C-h>', '<Left>', { noremap = true })
 vim.keymap.set('i', '<C-j>', '<Down>', { noremap = true })
 vim.keymap.set('i', '<C-k>', '<Up>', { noremap = true })
 vim.keymap.set('i', '<C-l>', '<Right>', { noremap = true })
 
--- Default actions in insert mode:
--- + <C-w> - delete word
--- + <C-f> - indent line automatically
--- ~ <C-d> -> <C-S-Tab> - indent line back
--- ~ <C-t> +> <C-Tab>   - indent line forward
--- ~ <C-u> -> <C-x>     - delete text before cursor
+-- Fast commenting below or above
+vim.keymap.set('n', 'gcj', 'o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add comment below' })
+vim.keymap.set('n', 'gck', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add comment above' })
+
+-- Save file
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = '[S]ave file' })
+-- Quit all
+vim.keymap.set('n', '<A-Esc>', '<cmd>qa<cr>', { desc = '[Q]uit All' })
 
 -- Remap some of the default insert commands:
 --  * <C-d> is used to scroll the completion menu down
 vim.keymap.set('i', '<C-S-Tab>', '<C-d>', { noremap = true })
 vim.keymap.set('i', '<C-Tab>', '<C-t>', { noremap = true })
+vim.keymap.set('i', '<C-d>', '<nop>')
+vim.keymap.set('i', '<C-t>', '<nop>')
 --  * <C-u> is used to scroll the completion menu up
 --  * <C-u> is also too close to <C-y>, which could lead to accidental presses
 vim.keymap.set({ 'i', 'c' }, '<C-x>', '<C-u>', { noremap = true })
@@ -174,11 +152,147 @@ vim.keymap.set({ 'i', 'c' }, '<C-u>', '<nop>')
 -- Overridden by multicursor.nvim
 vim.keymap.set('n', '<ESC>', '<cmd>nohlsearch<CR>')
 
-vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 -- Exit terminal mode in the builtin terminal with a bit easier shortcut
 -- NOTE: This may not work in all terminal emulators/tmux/etc
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Sync clipboard between OS and Neovim
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>c', '"+c', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>C', '"+C', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"+d', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>D', '"+D', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { noremap = true })
+
+vim.paste = (function(overridden)
+  return function(lines, phase)
+    local mode = vim.fn.mode()
+    if lines[#lines] == '' then
+      table.remove(lines, #lines)
+    end
+    if mode == 'n' then
+      vim.api.nvim_put(lines, 'l', true, false) -- paste
+    elseif mode == 'V' then
+      vim.fn.execute [[ exe "silent normal! \<Del>" ]] -- Delete selection
+      vim.api.nvim_put(lines, 'l', false, false) -- Paste
+      local select_end = vim.fn.getpos "']" -- Get selection end-position
+      select_end[3] = 0 -- Move cursor to start of line
+      vim.fn.setpos('.', select_end)
+    else
+      overridden(lines, phase)
+    end
+  end
+end)(vim.paste)
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+-- Add timestamp as extension for backup files
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = augroup 'timestamp_backupext',
+  desc = 'Add timestamp to backup extension',
+  pattern = '*',
+  callback = function()
+    vim.o.backupext = '-' .. vim.fn.strftime '%Y%m%d%H%M'
+  end,
+})
+
+-- Highlight when copying text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup 'highlight_yank',
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = augroup 'checktime',
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd 'checktime'
+    end
+  end,
+})
+
+-- Resize splits if window got resized
+vim.api.nvim_create_autocmd('VimResized', {
+  group = augroup 'resize_splits',
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd 'tabdo wincmd ='
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
+
+-- Close some filetypes with `q`
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close_with_q',
+  pattern = {
+    'PlenaryTestPopup',
+    'checkhealth',
+    'dbout',
+    'gitsigns-blame',
+    'grug-far',
+    'help',
+    'lspinfo',
+    'neotest-output',
+    'neotest-output-panel',
+    'neotest-summary',
+    'notify',
+    'qf',
+    'query',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+  },
+  callback = function(event)
+    vim.schedule(function()
+      if not vim.api.nvim_buf_is_valid(event.buf) then
+        return
+      end
+      vim.bo[event.buf].buflisted = false
+      vim.keymap.set('n', 'q', function()
+        vim.cmd 'close'
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, { buffer = event.buf, silent = true, desc = 'Quit buffer' })
+    end)
+  end,
+})
+
+-- Make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'man_unlisted',
+  pattern = 'man',
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
+
+-- Wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'wrap_spell',
+  pattern = { 'text', 'plaintex', 'typst', 'gitcommit', 'markdown' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = augroup 'auto_create_dir',
+  callback = function(event)
+    if event.match:match '^%w%w+:[\\/][\\/]' then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
 
 -- Install `lazy.nvim` plugin manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'

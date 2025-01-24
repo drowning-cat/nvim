9caa97e30930e7ad0aca74c79a721fb0afa7e02a
Drowning Cat <shamal.ma.personal@gmail.com>
2025-01-24 21:57:56 +0300

    feat: enable file backups

--- init.lua
+++ init.lua
@@ -16,6 +16,21 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Backup files
+-- Double slash to build file name from the complete path to the file with all path separators changed to percent '%' signs
+vim.opt.backupdir = vim.fn.stdpath 'state' .. '/backup'
+vim.opt.backup = true
+
+-- Add timestamp as extension for backup files
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('timestamp_backupext', { clear = true }),
+  desc = 'Add timestamp to backup extension',
+  pattern = '*',
+  callback = function()
+    vim.opt.backupext = '-' .. vim.fn.strftime '%Y%m%d%H%M'
+  end,
+})
+
 -- Sync clipboard between OS and Neovim
 -- vim.schedule(function()
 --   vim.opt.clipboard = 'unnamedplus'

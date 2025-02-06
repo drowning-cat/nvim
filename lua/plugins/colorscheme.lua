35b0e8d7682c80ccbdc5863e91866ee97841dc15
Drowning Cat <shamal.ma.personal@gmail.com>
2025-02-06 21:18:49 +0300

    feat: persistent colorscheme, remove huez.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,15 @@
--- To see already installed colorschemes use `:Telescope colorscheme`
-
 return {
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'tokyonight-night'
-  --   end,
-  -- },
-  -- {
-  --   'rebelot/kanagawa.nvim',
-  --   lazy = true,
-  --   opts = {
-  --     commentStyle = { italic = false },
-  --     keywordStyle = { italic = false },
-  --   },
-  -- },
-  { -- Theme manager
-    'vague2k/huez.nvim',
-    import = 'huez-manager.import',
-    event = 'UIEnter',
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { '<leader>tt', function() require('huez.pickers').themes() end, desc = '[T]oggle [T]heme' },
-      { '<leader>H', function() require('huez.pickers').themes() end, desc = '[H]uez' },
-      { '<leader>Hl', function() require('huez.pickers').live() end, desc = '[H]uez [L]ive' },
-      { '<leader>He', function() require('huez.pickers').ensured() end, desc = '[H]uez [E]nsured' },
-      { '<leader>Hf', function() require('huez.pickers').favorites() end, desc = '[H]uez [F]avorites' },
-    },
+  require('persist-colors').lazy_setup {
+    { 'folke/tokyonight.nvim' },
+    { 'RRethy/base16-nvim' },
+    { 'rebelot/kanagawa.nvim', opts = {
+      commentStyle = { italic = false },
+      keywordStyle = { italic = false },
+    } },
+    { 'rose-pine/neovim', name = 'rose-pine', opts = { styles = { italic = false } } },
+    { 'EdenEast/nightfox.nvim' },
   },
+
   { -- Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
     event = 'VimEnter',

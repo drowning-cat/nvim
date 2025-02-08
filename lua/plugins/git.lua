9c02a8b849950caa6ccdb2fc7a6f1cc1a968c746
Drowning Cat <shamal.ma.personal@gmail.com>
2025-02-08 15:28:49 +0300

    chore: update gitsigns config after v1.0.0

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -34,7 +34,7 @@ return {
         -- Navigation: previous hunk
         map('n', ']h', function()
           if vim.wo.diff then
-            vim.cmd.normal { ']c', bang = true }
+            vim.cmd.normal { ']h', bang = true }
           else
             gitsigns.nav_hunk 'next'
           end
@@ -43,7 +43,7 @@ return {
         -- Navigation: next hunk
         map('n', '[h', function()
           if vim.wo.diff then
-            vim.cmd.normal { '[c', bang = true }
+            vim.cmd.normal { '[h', bang = true }
           else
             gitsigns.nav_hunk 'prev'
           end
@@ -55,7 +55,6 @@ return {
         map('v', '<leader>gs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[G]it [s]tage hunk' })
         map('v', '<leader>gr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[G]it [r]eset hunk' })
         -- stylua: ignore end
-        map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = '[G]it [u]ndo stage hunk' })
         map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[G]it [S]tage buffer' })
         map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[G]it [R]eset buffer' })
         map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[G]it [p]review hunk' })
@@ -65,8 +64,7 @@ return {
         -- stylua: ignore
         map('n', '<leader>gD', function() gitsigns.diffthis '@' end, { desc = '[G]it [D]iff against last commit' })
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git [b]lame line' })
-        map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git [D]eleted' })
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '[G]it select hunk' })
+        map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, { desc = '[G]it select hunk' })
       end,
     },
   },

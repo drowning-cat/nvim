8a7616524a38dc122ff748af0e74a7690cea79a6
Drowning Cat <shamal.ma.personal@gmail.com>
2025-03-08 00:50:53 +0300

    fix: override buffer keymaps with submode.nvim keymaps

--- lua/plugins/window.lua
+++ lua/plugins/window.lua
@@ -96,8 +96,72 @@ return {
     'pogyomo/submode.nvim',
     keys = { '<C-w>r', '<C-w><C-r>' },
     config = function()
+      local submode = require 'submode'
+
+      ---@alias RegKey [string, string|function, vim.keymap.set.Opts]
+      ---@type table<string, RegKey>
+      local reg_state = {}
+
+      ---@type fun(mode: string, lhs: string, rhs: string|function, opts?: vim.keymap.set.Opts)
+      local reg = function(mode, lhs, rhs, opts)
+        reg_state[mode] = reg_state[mode] or {}
+        reg_state[mode][#reg_state[mode] + 1] = { lhs, rhs, opts or {} }
+      end
+
+      ---@return integer[]
+      local ft_include = { 'snacks_picker_list', 'snacks_picker_input' }
+      local leave_bufs = function()
+        return vim
+          .iter(submode.state.leave_bufs)
+          :filter(function(buf)
+            return vim.api.nvim_buf_is_valid(buf) and vim.list_contains(ft_include, vim.bo[buf].ft)
+          end)
+          :totable()
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'SubmodeEnterPost',
+        callback = function(event)
+          local mode = event.data.name ---@type string
+          local bufs = leave_bufs()
+          ---@param key RegKey
+          vim.iter(reg_state[mode]):each(function(key)
+            local lhs, rhs, opts = unpack(key)
+            submode.set(mode, lhs, rhs, opts)
+            for _, buf in ipairs(bufs) do
+              submode.set(mode, lhs, rhs, vim.tbl_extend('keep', opts, { buffer = buf }))
+            end
+          end)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'SubmodeLeavePre',
+        callback = function(event)
+          local mode = event.data.name ---@type string
+          local bufs = leave_bufs()
+          ---@param key RegKey
+          vim.iter(reg_state[mode]):each(function(key)
+            local lhs, _, _ = unpack(key)
+            submode.del(mode, lhs)
+            for _, buf in ipairs(bufs) do
+              submode.del(mode, lhs, { buffer = buf })
+            end
+          end)
+        end,
+      })
+
+      ---Patched resize function to redraw vim.notify messages during statusline resizing
+      ---@param dir Direction
+      local resize_patched = function(dir)
+        return function()
+          win.resize(dir)
+          vim.cmd 'messages'
+        end
+      end
+
       -- Resize
-      require('submode').create('WinResize', {
+      submode.create('WinResize', {
         mode = 'n',
         enter = { '<C-w>r', '<C-w><C-r>' },
         leave = { '<Esc>', 'q', '<C-c>' },
@@ -109,23 +173,15 @@ return {
             vim.u.notify('', { force = false })
           end,
         },
-        default = function(register)
-          ---Patched resize function to redraw vim.notify messages during statusline resizing
-          ---@param dir Direction
-          local resize_patched = function(dir)
-            return function()
-              win.resize(dir)
-              vim.cmd 'messages'
-            end
-          end
-          register('h', resize_patched 'left', { desc = 'Resize left' })
-          register('j', resize_patched 'down', { desc = 'Resize down' })
-          register('k', resize_patched 'up', { desc = 'Resize up' })
-          register('l', resize_patched 'right', { desc = 'Resize right' })
-          register('<Left>', resize_patched 'left', { desc = 'Resize left' })
-          register('<Down>', resize_patched 'down', { desc = 'Resize down' })
-          register('<Up>', resize_patched 'up', { desc = 'Resize up' })
-          register('<Right>', resize_patched 'right', { desc = 'Resize right' })
+        default = function()
+          reg('WinResize', 'h', resize_patched 'left', { desc = 'Resize left' })
+          reg('WinResize', 'j', resize_patched 'down', { desc = 'Resize down' })
+          reg('WinResize', 'k', resize_patched 'up', { desc = 'Resize up' })
+          reg('WinResize', 'l', resize_patched 'right', { desc = 'Resize right' })
+          reg('WinResize', '<Left>', resize_patched 'left', { desc = 'Resize left' })
+          reg('WinResize', '<Down>', resize_patched 'down', { desc = 'Resize down' })
+          reg('WinResize', '<Up>', resize_patched 'up', { desc = 'Resize up' })
+          reg('WinResize', '<Right>', resize_patched 'right', { desc = 'Resize right' })
         end,
       })
     end,

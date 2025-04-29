9cb30a34a33c68d36b4bebc8025f1076a11fe8b4
Drowning Cat <shamal.ma.personal@gmail.com>
2025-04-29 16:18:57 +0300

    feat: adjust win/tab keymaps

--- lua/plugins/wintab.lua
+++ lua/plugins/wintab.lua
@@ -1,28 +1,42 @@
 local wins = require 'misc.wins'
 
--- Move
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus left' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus down' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus up' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus right' })
-vim.keymap.set('n', '<C-Left>', '<C-w>h', { desc = 'Move focus left' })
-vim.keymap.set('n', '<C-Down>', '<C-w>j', { desc = 'Move focus down' })
-vim.keymap.set('n', '<C-Up>', '<C-w>k', { desc = 'Move focus up' })
-vim.keymap.set('n', '<C-Right>', '<C-w>l', { desc = 'Move focus right' })
+-- Vim tabs
+vim.keymap.set('n', 'gC', '<cmd>tabclose<CR>', { desc = 'Vimtab [C]lose' })
+vim.keymap.set('n', 'gN', '<cmd>tabnew<CR>', { desc = 'Vimtab [N]ew' })
 
+-- Create
+vim.keymap.set('n', '<leader>V', '<cmd>vnew<CR>', { desc = 'Open [V]ertical window' })
+vim.keymap.set('n', '<leader>H', '<cmd>new<CR>', { desc = 'Open [H]orizontal window' })
+
+vim.keymap.set('n', '<leader>w', '<C-w>', { noremap = true })
 -- Close
-vim.keymap.set('n', '<C-q>', wins.close, { desc = 'Close the window' })
-
+vim.keymap.set('n', '<leader>wq', wins.close, { desc = 'Close the window' })
 -- Swap
 -- stylua: ignore start
-vim.keymap.set('n', '<C-S-h>', function() wins.swap_buf 'left' end, { desc = 'Swap with buffer left' })
-vim.keymap.set('n', '<C-S-j>', function() wins.swap_buf 'down' end, { desc = 'Swap with buffer down' })
-vim.keymap.set('n', '<C-S-k>', function() wins.swap_buf 'up' end, { desc = 'Swap with buffer up' })
-vim.keymap.set('n', '<C-S-l>', function() wins.swap_buf 'right' end, { desc = 'Swap with buffer right' })
-vim.keymap.set('n', '<C-S-Left>', function() wins.swap_buf 'left' end, { desc = 'Swap with buffer left' })
-vim.keymap.set('n', '<C-S-Down>', function() wins.swap_buf 'down' end, { desc = 'Swap with buffer down' })
-vim.keymap.set('n', '<C-S-Up>', function() wins.swap_buf 'up' end, { desc = 'Swap with buffer up' })
-vim.keymap.set('n', '<C-S-Right>', function() wins.swap_buf 'right' end, { desc = 'Swap with buffer right' })
+vim.keymap.set('n', '<leader>wH', function() wins.swap_buf 'h' end, { desc = 'Swap with buffer left' })
+vim.keymap.set('n', '<leader>wJ', function() wins.swap_buf 'j' end, { desc = 'Swap with buffer down' })
+vim.keymap.set('n', '<leader>wK', function() wins.swap_buf 'k' end, { desc = 'Swap with buffer up' })
+vim.keymap.set('n', '<leader>wL', function() wins.swap_buf 'l' end, { desc = 'Swap with buffer right' })
+-- stylua: ignore end
+-- Navigate
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-Left>', '<C-w>h')
+vim.keymap.set('n', '<C-Down>', '<C-w>j')
+vim.keymap.set('n', '<C-Up>', '<C-w>k')
+vim.keymap.set('n', '<C-Right>', '<C-w>l')
+-- Swap
+-- stylua: ignore start
+vim.keymap.set('n', '<C-S-h>', function() wins.swap_buf 'h' end)
+vim.keymap.set('n', '<C-S-j>', function() wins.swap_buf 'j' end)
+vim.keymap.set('n', '<C-S-k>', function() wins.swap_buf 'k' end)
+vim.keymap.set('n', '<C-S-l>', function() wins.swap_buf 'l' end)
+vim.keymap.set('n', '<C-S-Left>', function() wins.swap_buf 'h' end)
+vim.keymap.set('n', '<C-S-Down>', function() wins.swap_buf 'j' end)
+vim.keymap.set('n', '<C-S-Up>', function() wins.swap_buf 'k' end)
+vim.keymap.set('n', '<C-S-Right>', function() wins.swap_buf 'l' end)
 -- stylua: ignore end
 
 return {
@@ -52,35 +66,48 @@ return {
       maximum_padding = 3,
     },
     keys = {
+      { '<leader><Tab>', desc = 'Tab' },
+      { '<leader><Tab><Tab>', desc = 'VimTab' },
       {
-        '<leader><Tab><Tab>',
+        '<leader><Tab>t',
         function()
           local buffers = require('barbar.state').buffers
           if not vim.tbl_isempty(buffers) then
             vim.o.showtabline = vim.o.showtabline ~= 0 and 0 or 2
           end
         end,
-        desc = '[T]oggle [T]ab bar',
+        desc = 'Tab [T]oggle',
       },
       { '[b', '<cmd>BufferPrevious<CR>', desc = '[B]uffer next' },
       { ']b', '<cmd>BufferNext<CR>', desc = '[B]uffer prev' },
       { '<leader>bd', '<cmd>BufferClose<CR>', desc = '[B]uffer [d]elete' },
-      { '<leader><Tab>h', '<cmd>BufferPrevious<CR>', desc = '[T]ab select left' },
-      { '<leader><Tab>l', '<cmd>BufferNext<CR>', desc = '[T]ab select right' },
-      { '<leader><Tab>k', '<cmd>BufferMoveNext<CR>', desc = '[T]ab move next' },
-      { '<leader><Tab>j', '<cmd>BufferMovePrevious<CR>', desc = '[T]ab move prev' },
-      { '<leader><Tab>p', '<cmd>BufferPin<CR>', desc = '[T]ab [P]in' },
-      { '<leader><Tab>q', '<cmd>BufferClose<CR>', desc = '[T]ab [q]uit' },
-      { '<leader><Tab>w', '<cmd>BufferCloseAllButCurrentOrPinned<CR>', desc = '[T]ab [w]ipeout tabs' },
-      { '<leader><Tab>1', '<Cmd>BufferGoto 1<CR>', desc = 'Tab [1]' },
-      { '<leader><Tab>2', '<Cmd>BufferGoto 2<CR>', desc = 'Tab [2]' },
-      { '<leader><Tab>3', '<Cmd>BufferGoto 3<CR>', desc = 'Tab [3]' },
-      { '<leader><Tab>4', '<Cmd>BufferGoto 4<CR>', desc = 'Tab [4]' },
-      { '<leader><Tab>5', '<Cmd>BufferGoto 5<CR>', desc = 'Tab [5]' },
-      { '<leader><Tab>6', '<Cmd>BufferGoto 6<CR>', desc = 'Tab [6]' },
-      { '<leader><Tab>7', '<Cmd>BufferGoto 7<CR>', desc = 'Tab [7]' },
-      { '<leader><Tab>8', '<Cmd>BufferGoto 8<CR>', desc = 'Tab [8]' },
-      { '<leader><Tab>9', '<Cmd>BufferGoto 9<CR>', desc = 'Tab [9]' },
+      { '<leader><Tab>h', '<cmd>BufferPrevious<CR>', desc = 'Tab select left' },
+      { '<leader><Tab>l', '<cmd>BufferNext<CR>', desc = 'Tab select right' },
+      { '<leader><Tab>k', '<cmd>BufferMoveNext<CR>', desc = 'Tab move next' },
+      { '<leader><Tab>j', '<cmd>BufferMovePrevious<CR>', desc = 'Tab move prev' },
+      { '<leader><Tab>p', '<cmd>BufferPin<CR>', desc = 'Tab [p]in' },
+      { '<leader><Tab>q', '<cmd>BufferClose<CR>', desc = 'Tab [q]uit' },
+      { '<leader><Tab>w', '<cmd>BufferCloseAllButCurrentOrPinned<CR>', desc = 'Tab [W]ipeout tabs' },
+      {
+        '<leader><Tab>c',
+        function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_get_name(buf) == '' then
+              vim.api.nvim_buf_delete(buf, {})
+            end
+          end
+        end,
+        desc = 'Tab [c]lean unnamed buffers',
+      },
+      { '<leader><Tab>1', '<Cmd>BufferGoto 1<CR>', desc = 'Tab goto [1]' },
+      { '<leader><Tab>2', '<Cmd>BufferGoto 2<CR>', desc = 'Tab goto [2]' },
+      { '<leader><Tab>3', '<Cmd>BufferGoto 3<CR>', desc = 'Tab goto [3]' },
+      { '<leader><Tab>4', '<Cmd>BufferGoto 4<CR>', desc = 'Tab goto [4]' },
+      { '<leader><Tab>5', '<Cmd>BufferGoto 5<CR>', desc = 'Tab goto [5]' },
+      { '<leader><Tab>6', '<Cmd>BufferGoto 6<CR>', desc = 'Tab goto [6]' },
+      { '<leader><Tab>7', '<Cmd>BufferGoto 7<CR>', desc = 'Tab goto [7]' },
+      { '<leader><Tab>8', '<Cmd>BufferGoto 8<CR>', desc = 'Tab goto [8]' },
+      { '<leader><Tab>9', '<Cmd>BufferGoto 9<CR>', desc = 'Tab goto [9]' },
       { '<leader><Tab>0', '<Cmd>BufferLast<CR>', desc = 'Tab last' },
     },
   },
@@ -167,14 +194,14 @@ return {
         },
         -- stylua: ignore
         default = function()
-          reg('WinResize', 'h', function() wins.resize 'left' end, { desc = 'Resize left' })
-          reg('WinResize', 'j', function() wins.resize 'down' end, { desc = 'Resize down' })
-          reg('WinResize', 'k', function() wins.resize 'up' end, { desc = 'Resize up' })
-          reg('WinResize', 'l', function() wins.resize 'right' end, { desc = 'Resize right' })
-          reg('WinResize', '<Left>', function() wins.resize 'left' end, { desc = 'Resize left' })
-          reg('WinResize', '<Down>', function() wins.resize 'down' end, { desc = 'Resize down' })
-          reg('WinResize', '<Up>', function() wins.resize 'up' end, { desc = 'Resize up' })
-          reg('WinResize', '<Right>', function() wins.resize 'right' end, { desc = 'Resize right' })
+          reg('WinResize', 'h', function() wins.resize 'h' end, { desc = 'Resize left' })
+          reg('WinResize', 'j', function() wins.resize 'j' end, { desc = 'Resize down' })
+          reg('WinResize', 'k', function() wins.resize 'k' end, { desc = 'Resize up' })
+          reg('WinResize', 'l', function() wins.resize 'l' end, { desc = 'Resize right' })
+          reg('WinResize', '<Left>', function() wins.resize 'h' end, { desc = 'Resize left' })
+          reg('WinResize', '<Down>', function() wins.resize 'j' end, { desc = 'Resize down' })
+          reg('WinResize', '<Up>', function() wins.resize 'k' end, { desc = 'Resize up' })
+          reg('WinResize', '<Right>', function() wins.resize 'l' end, { desc = 'Resize right' })
         end,
       })
     end,

0cbca1050835b2c34bab7e02de83e881be9bfdb1
Drowning Cat <shamal.ma.personal@gmail.com>
2025-12-02 21:07:45 +0300

    patch: hardcode tokyonight colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,14 +1,22 @@
 return {
-  require('misc.save_colors').lazy_setup {
-    { 'folke/tokyonight.nvim' },
-    { 'RRethy/base16-nvim' },
-    { 'rebelot/kanagawa.nvim', opts = {
-      commentStyle = { italic = false },
-      keywordStyle = { italic = false },
-    } },
-    { 'rose-pine/neovim', name = 'rose-pine', opts = { styles = { italic = false } } },
-    { 'EdenEast/nightfox.nvim' },
+  {
+    'folke/tokyonight.nvim',
+    config = function(_, opts)
+      require('tokyonight').setup(opts)
+      vim.cmd.colorscheme 'tokyonight'
+    end,
   },
+
+  -- require('misc.save_colors').lazy_setup {
+  --   { 'folke/tokyonight.nvim' },
+  --   { 'RRethy/base16-nvim' },
+  --   { 'rebelot/kanagawa.nvim', opts = {
+  --     commentStyle = { italic = false },
+  --     keywordStyle = { italic = false },
+  --   } },
+  --   { 'rose-pine/neovim', name = 'rose-pine', opts = { styles = { italic = false } } },
+  --   { 'EdenEast/nightfox.nvim' },
+  -- },
 
   { -- Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
